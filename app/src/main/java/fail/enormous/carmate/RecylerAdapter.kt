505f63af3a57bde35d004a141fb40bb36582cc75
Jacob <jacob.goodridge@hotmail.com>
2020-11-30 10:35:36 +1100

    UI fixed

--- app/src/main/java/fail/enormous/carmate/RecylerAdapter.kt
+++ app/src/main/java/fail/enormous/carmate/RecylerAdapter.kt
@@ -1,6 +1,7 @@
 package fail.enormous.carmate
 
 import android.content.Context
+import android.provider.Settings.Global.getString
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 class RecyclerAdapter(private val context: Context, private val listRecyclerItem: List<Any>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val brand: TextView = itemView.findViewById<View>(R.id.brand) as TextView
-        val model: TextView = itemView.findViewById<View>(R.id.model) as TextView
+        // val model: TextView = itemView.findViewById<View>(R.id.model) as TextView
         val year: TextView = itemView.findViewById<View>(R.id.year) as TextView
         val color: TextView = itemView.findViewById<View>(R.id.color) as TextView
         val type: TextView = itemView.findViewById<View>(R.id.type) as TextView
@@ -40,28 +41,49 @@ class RecyclerAdapter(private val context: Context, private val listRecyclerItem
             TYPE -> {
                 val itemViewHolder = viewHolder as ItemViewHolder
                 val carlist: CarList = listRecyclerItem[i] as CarList
-                itemViewHolder.brand.setText(carlist.brand)
+                itemViewHolder.brand.setText(carlist.brand + " " + carlist.model)
+                itemViewHolder.year.setText(context.getString(R.string.year_title, carlist.year))
+                itemViewHolder.color.setText(context.getString(R.string.color_title, capitaliseString(carlist.color)))
+                itemViewHolder.type.setText(context.getString(R.string.type_title, capitaliseString(carlist.type)))
+                itemViewHolder.price.setText(context.getString(R.string.price_title, "$" + carlist.price))
+                /* itemViewHolder.brand.setText(carlist.brand)
                 itemViewHolder.model.setText(carlist.model)
                 itemViewHolder.year.setText(carlist.year)
                 itemViewHolder.color.setText(carlist.color)
                 itemViewHolder.type.setText(carlist.type)
-                itemViewHolder.price.setText(carlist.price)
+                itemViewHolder.price.setText(carlist.price) */
             }
             else -> {
                 val itemViewHolder = viewHolder as ItemViewHolder
                 val carlist: CarList = listRecyclerItem[i] as CarList
-                itemViewHolder.brand.setText(carlist.brand)
+                itemViewHolder.brand.setText(carlist.brand + " " + carlist.model)
+                itemViewHolder.year.setText(context.getString(R.string.year_title, carlist.year))
+                itemViewHolder.color.setText(context.getString(R.string.color_title, capitaliseString(carlist.color)))
+                itemViewHolder.type.setText(context.getString(R.string.type_title, capitaliseString(carlist.type)))
+                itemViewHolder.price.setText(context.getString(R.string.price_title, "$" + carlist.price))
+                /* itemViewHolder.brand.setText(carlist.brand)
                 itemViewHolder.model.setText(carlist.model)
                 itemViewHolder.year.setText(carlist.year)
                 itemViewHolder.color.setText(carlist.color)
                 itemViewHolder.type.setText(carlist.type)
-                itemViewHolder.price.setText(carlist.price)
+                itemViewHolder.price.setText(carlist.price) */
             }
         }
     }
 
     override fun getItemCount(): Int {
         return listRecyclerItem.size
+    }
+
+    private fun capitaliseString(str: String): String {
+        var stringy = str
+        try {
+            stringy = str.substring(0, 1).toUpperCase() + str.substring(1) // Make first letter a capital
+        }
+        catch (e: Exception) {
+            // If string is null, index out of bounds exception is caught
+        }
+        return stringy
     }
 
     companion object {
